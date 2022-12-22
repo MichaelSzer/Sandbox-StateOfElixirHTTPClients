c43a69d1b6f492e9da3d271fbd48dbaa7fc62276
MichaelSzer <michaelszer@gmail.com>
2022-12-22 14:27:55 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# FunctionalLangs
+
+Elixir application to test the MINT Http Client.
+
+Based on: https://blog.appsignal.com/2020/07/28/the-state-of-elixir-http-clients.html
---
+++ lib/functional_langs.ex
@@ -0,0 +1,18 @@
+defmodule FunctionalLangs do
+  @moduledoc """
+  Documentation for `FunctionalLangs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FunctionalLangs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
